c7bac90ade48112d215cfde66d27c9b0929ffce1
Brook-B-Nigatu <burabalke7776@gmail.com>
2022-10-06 12:00:49 +0200

    Initial commit

---
+++ td3.cpp
@@ -0,0 +1,157 @@
+#include <iostream>
+#include "td3.hpp"
+#include "support.hpp"
+#include <stdlib.h>
+#include <math.h>       // sin, cos
+#include <assert.h>
+
+using namespace std;
+
+using namespace support;
+
+double* extend_array(double* array, int length, int new_size) {
+  // IMPLEMENT YOUR FUNCTION HERE
+    double *new_arr = new double[new_size];
+    for (int i = 0; i < new_size; i++){
+        if (i < length){
+            new_arr[i] = array[i];
+        }
+        else{
+            new_arr[i] = 0;
+        }
+    }
+    delete[] array;
+    return new_arr;
+}
+
+double* shrink_array(double* array, int length, int new_size) {
+  // IMPLEMENT YOUR FUNCTION HERE
+    double *new_arr = new double[new_size];
+    for (int i = 0; i < new_size; i++){
+        new_arr[i] = array[i];
+    }
+    delete[] array;
+    return new_arr;
+}
+
+double* append_to_array(double element,
+                        double* array,
+                        int &current_size,
+                        int &max_size) {
+  // IMPLEMENT YOUR FUNCTION HERE
+    if (current_size == max_size){
+        array = extend_array(array, max_size, max_size + 5);
+        max_size += 5;
+    }
+    array[current_size] = element;
+    current_size += 1;
+    return array;
+}
+
+double* remove_from_array(double* array,
+                          int &current_size,
+                          int &max_size) {
+  // IMPLEMENT YOUR FUNCTION HERE
+    current_size--;
+    if ((max_size - current_size) >= 5){
+        array = shrink_array(array, current_size, max_size - 5);
+        max_size -= 5;
+    }
+    return array;
+}
+
+bool simulate_projectile(const double magnitude, const double angle,
+                         const double simulation_interval,
+                         double *targets, int &tot_targets,
+                         int *obstacles, int tot_obstacles,
+                         double* &telemetry,
+                         int &telemetry_current_size,
+                         int &telemetry_max_size) {
+  // YOU CAN MODIFY THIS FUNCTION TO RECORD THE TELEMETRY
+
+  bool hit_target, hit_obstacle;
+  double v0_x, v0_y, x, y, t;
+  double PI = 3.14159265;
+  double g = 9.8;
+
+  v0_x = magnitude * cos(angle * PI / 180);
+  v0_y = magnitude * sin(angle * PI / 180);
+
+  t = 0;
+  x = 0;
+  y = 0;
+  telemetry = append_to_array(t, telemetry, telemetry_current_size, telemetry_max_size);
+  telemetry = append_to_array(x, telemetry, telemetry_current_size, telemetry_max_size);
+  telemetry = append_to_array(y, telemetry, telemetry_current_size, telemetry_max_size);
+  hit_target = false;
+  hit_obstacle = false;
+  while (y >= 0 && (! hit_target) && (! hit_obstacle)) {
+    double * target_coordinates = find_collision(x, y, targets, tot_targets);
+    if (target_coordinates != NULL) {
+      remove_target(targets, tot_targets, target_coordinates);
+      hit_target = true;
+    } else if (find_collision(x, y, obstacles, tot_obstacles) != NULL) {
+      hit_obstacle = true;
+    } else {
+      t = t + simulation_interval;
+      y = v0_y * t  - 0.5 * g * t * t;
+      x = v0_x * t;
+      if (y >= 0){
+          telemetry = append_to_array(t, telemetry, telemetry_current_size, telemetry_max_size);
+          telemetry = append_to_array(x, telemetry, telemetry_current_size, telemetry_max_size);
+          telemetry = append_to_array(y, telemetry, telemetry_current_size, telemetry_max_size);
+      }
+
+    }
+  }
+
+  return hit_target;
+}
+
+void merge_telemetry(double **telemetries,
+                     int tot_telemetries,
+                     int *telemetries_sizes,
+                     double* &global_telemetry,
+                     int &global_telemetry_current_size,
+                     int &global_telemetry_max_size) {
+  // IMPLEMENT YOUR FUNCTION HERE
+    int elements_from_each[tot_telemetries];
+    for (int i = 0; i < tot_telemetries; i++){
+        elements_from_each[i] = 0;
+    }
+    bool end_loop = false;
+    while(!end_loop){
+        int min_index = -1;
+        end_loop = true;
+        for(int i = 0; i < tot_telemetries; i++){
+            if(elements_from_each[i] < telemetries_sizes[i]){
+                end_loop = false;
+                if (min_index == -1 || telemetries[i][elements_from_each[i]] < telemetries[min_index][elements_from_each[min_index]]){
+                    min_index = i;
+                }
+            }
+        }
+        if(!(min_index == -1)){
+            global_telemetry = append_to_array(telemetries[min_index][elements_from_each[min_index]], global_telemetry, global_telemetry_current_size, global_telemetry_max_size);
+            global_telemetry = append_to_array(telemetries[min_index][elements_from_each[min_index] + 1], global_telemetry, global_telemetry_current_size, global_telemetry_max_size);
+            global_telemetry = append_to_array(telemetries[min_index][elements_from_each[min_index] + 2], global_telemetry, global_telemetry_current_size, global_telemetry_max_size);
+            elements_from_each[min_index] += 3;
+        }
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
